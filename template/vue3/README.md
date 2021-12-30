3e554e0694bc840459874856accded798bc3c786
shuaisguo <shuaisguo@tencent.com>
2021-12-30 11:12:37 +0800

    fix: vue模版安装依赖错误

--- template/vue3/README.md
+++ template/vue3/README.md
@@ -5,13 +5,13 @@
 ```
 developing.script/developing.js http://127.0.0.1:9000/index.js
 developing.style/developing.css http://127.0.0.1:9000/index.css
-wss://iot.cloud.tencent.com/ wss://localhost:9000/
+$wss://iot.cloud.tencent.com/ wss://localhost:9000/ # 代理 HMR
 ```
 
 
 ### dev
 ```bash
-npm run dev # 将会生成 http://127.0.0.1:9000/index.js http://127.0.0.1:9000/index.css
+npm run dev -- --productId=your_productId # 将会生成 http://127.0.0.1:9000/index.js http://127.0.0.1:9000/index.css
 
 ```
 
--- template/vue3/src/index.js
+++ template/vue3/src/index.js
@@ -4,7 +4,7 @@ import { Button, Cell, Icon } from 'vant';
 import sdk from 'qcloud-iotexplorer-h5-panel-sdk';
 import router from './router';
 import App from './App.vue';
-import { StandardDeviceAdapter4H5 as StandardDeviceAdapter, utils } from 'qcloud-iotexplorer-bluetooth-adapter-llsync';
+import { StandardDeviceAdapter4H5 as StandardDeviceAdapter } from 'qcloud-iotexplorer-bluetooth-adapter-llsync';
 
 import 'vant/lib/index.css';
 
@@ -12,10 +12,6 @@ StandardDeviceAdapter.injectOptions({ appDevSdk: sdk.appDevSdk });
 sdk.blueToothAdapter.addAdapter(StandardDeviceAdapter, true);
 
 const vConsole = new VConsole();
-
-// 由于 vConsole 会劫持浏览器的 console，这里需要重新赋值一下，才能正常打印
-utils.log.log = console.log;
-localStorage.debug = '*';
 
 const app = createApp(App);
 [router, Button, Cell, Icon].forEach(plugin => app.use(plugin));
